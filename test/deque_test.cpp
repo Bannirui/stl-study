3e5eff91ed90b8bbc890189c6d657f797757571b
dingrui <Bannirui@outlook.com>
2026-01-04 14:53:19 +0800

    deque构造 初始化列表

--- test/deque_test.cpp
+++ test/deque_test.cpp
@@ -4,14 +4,24 @@
 
 #include <iostream>
 
-int main() {
-    {
-        std::deque<int> d;
+template<typename T>
+std::ostream &operator<<(std::ostream &s, const std::deque<T> &v) {
+    s.put('{');
+    for (char comma[]{'\0', ' ', '\0'}; const auto &e: v) {
+        s << comma << e, comma[0] = ',';
     }
+    return s << "}\n";
+}
+
+int main() {
     {
         // initializer list构造
         std::deque<int> d = {7, 5, 16, 8};
-        d.push_back(1);
+        std::cout << d << std::endl;
+    }
+    {
+        // 无参
+        std::deque<int> d;
     }
     return 0;
 }
